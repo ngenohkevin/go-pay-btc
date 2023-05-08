7b9236f66635c5db7721af390076f29c298230a8
ngenohkevin <ngenohkevin19@gmail.com>
2023-05-08 17:14:21 +0300

    add config files

--- cmd/gopaybtc/main.go
+++ cmd/gopaybtc/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"github.com/ngenohkevin/go-pay-btc/pkg/gopaybtc"
+	"log"
+)
+
+func main() {
+
+	config, err := gopaybtc.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	server, err := gopaybtc.NewServer(config.ApiKey)
+	if err != nil {
+		log.Fatal("Cannot create server", err)
+	}
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Unable to start the server address", err)
+	}
+
+}
